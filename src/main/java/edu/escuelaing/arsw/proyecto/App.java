a522e3b8ef411b43dff875627a3cd1b7967a85a8
mateomejia401@gmail.com <mateomejia401@gmail.com>
2022-07-19 11:27:06 -0500

    Tercer intento de despliegue

--- src/main/java/edu/escuelaing/arsw/proyecto/App.java
+++ src/main/java/edu/escuelaing/arsw/proyecto/App.java
@@ -4,10 +4,20 @@ package edu.escuelaing.arsw.proyecto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
-public class App 
-{
-    public static void main(String[] args){
-        SpringApplication.run(App.class, args);
+public class App {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(App.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
+        app.run(args);
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 8080; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
 }
